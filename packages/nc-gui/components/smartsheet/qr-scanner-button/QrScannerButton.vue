7120e56284b9a920d32be80e74c72314c9438b6c
Daniel Spaude <daniel@spaude.de>
2022-12-29 13:30:58 +0100

    QR Scanner: little refactoring and clenaup

--- packages/nc-gui/components/smartsheet/qr-scanner-button/QrScannerButton.vue
+++ packages/nc-gui/components/smartsheet/qr-scanner-button/QrScannerButton.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import type { SelectProps } from 'ant-design-vue'
-import { UITypes } from 'nocodb-sdk'
 import { ref } from 'vue'
 import { StreamBarcodeReader } from 'vue-barcode-reader'
 import { NOCO } from '#imports'
@@ -18,10 +17,6 @@ const view = inject(ActiveViewInj, ref())
 
 const qrCodeFieldOptions = ref<SelectProps['options']>([])
 
-// interface Entry {
-//   name: string
-// }
-
 onBeforeMount(init)
 
 async function init() {
@@ -33,38 +28,55 @@ async function init() {
   })
 }
 
-const showQrCodeScanner = ref(false)
-// const entry = ref<Entry | null>(null)
+const showQrCodeScannerOverlay = ref(false)
 
 const selectedCodeColumnIdToScanFor = ref('')
+const lastScannedQrCode = ref('')
+
+const scannerIsReady = ref(false)
+
+const onLoaded = async () => {
+  scannerIsReady.value = true
+}
+
+const showScannerField = computed(() => scannerIsReady.value && selectedCodeColumnIdToScanFor.value !== '')
+const showPleaseSelectColumnMessage = computed(() => !selectedCodeColumnIdToScanFor.value)
+const showScannerIsLoadingMessage = computed(() => selectedCodeColumnIdToScanFor.value && !scannerIsReady.value)
 
 const onDecode = async (qrCodeValue: string) => {
-  showQrCodeScanner.value = false
+  if (!showScannerField.value || qrCodeValue === lastScannedQrCode.value) {
+    return
+  }
   try {
     const nameOfSelectedColumnToScanFor = meta.value?.columns?.find(
       (column) => column.id === selectedCodeColumnIdToScanFor.value,
     )?.title
     const whereClause = `(${nameOfSelectedColumnToScanFor},eq,${qrCodeValue})`
-    // const foundRowsForQrCode = await $api.dbViewRow.findOne(NOCO, project.value!.id!, meta.value!.id!, view.value!.title!, {
     const foundRowsForQrCode = (
       await $api.dbViewRow.list(NOCO, project.value!.id!, meta.value!.id!, view.value!.title!, {
         where: whereClause,
       })
     ).list
 
-    if (foundRowsForQrCode.length === 0) {
-      // extract into localisation file
-      message.info('No row found for this QR code')
-      showQrCodeScanner.value = true
-      return
-    } else if (foundRowsForQrCode.length > 1) {
-      // TODO: improve this message and extract into localisation file
-      message.warn('More than one row found for this QR code. Currently only unique QR codes are supported.')
-      showQrCodeScanner.value = true
+    if (foundRowsForQrCode.length !== 1) {
+      showQrCodeScannerOverlay.value = true
+      lastScannedQrCode.value = qrCodeValue
+      setTimeout(() => {
+        lastScannedQrCode.value = ''
+      }, 4000)
+      if (foundRowsForQrCode.length === 0) {
+        // extract into localisation file
+        message.info(`No row found for this QR code for column '${nameOfSelectedColumnToScanFor}'.`)
+      }
+      if (foundRowsForQrCode.length > 1) {
+        // extract into localisation file
+        message.warn('More than one row found for this QR code. Currently only unique QR codes are supported.')
+      }
       return
     }
 
-    message.info('Found row for this QR code - opening edit mode...')
+    showQrCodeScannerOverlay.value = false
+    lastScannedQrCode.value = ''
     const primaryKeyValueForFoundRow = extractPkFromRow(foundRowsForQrCode[0], meta!.value!.columns!)
 
     router.push({
@@ -78,27 +90,19 @@ const onDecode = async (qrCodeValue: string) => {
   }
 }
 
-const scannerIsReady = ref(false)
-
-const onLoaded = async () => {
-  scannerIsReady.value = true
-}
-
-// TODO: ensure that when modal is closed, scannerIsReady gets set back to false
 </script>
 
 <template>
   <div>
-    <a-button class="nc-btn-share-view nc-toolbar-btn" @click="showQrCodeScanner = true">
+    <a-button class="nc-btn-share-view nc-toolbar-btn" @click="showQrCodeScannerOverlay = true">
       <div class="flex items-center gap-1">
         <QrCodeScan />
-        <!-- Share View -->
         <span class="!text-sm font-weight-normal"> {{ $t('activity.scanQrCode') }}</span>
       </div>
     </a-button>
     <a-modal
-      v-model:visible="showQrCodeScanner"
-      :class="{ active: showQrCodeScanner }"
+      v-model:visible="showQrCodeScannerOverlay"
+      :class="{ active: showQrCodeScannerOverlay }"
       :closable="false"
       width="28rem"
       centered
@@ -107,7 +111,7 @@ const onLoaded = async () => {
       destroy-on-close
     >
       <div class="relative flex flex-col h-full">
-        <a-form-item :label="$t('labels.qrCodeColumn')">
+        <a-form-item :label="$t('labels.columnToScanFor')">
           <a-select
             v-model:value="selectedCodeColumnIdToScanFor"
             class="w-full"
@@ -117,14 +121,16 @@ const onLoaded = async () => {
           />
         </a-form-item>
 
-        <!-- <qrcode-stream v-if="showQrCodeScanner" @decode="onDecode" style="width: 100%; height: 100%"></qrcode-stream> -->
         <div>
-          <StreamBarcodeReader v-show="scannerIsReady" @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
-          <div v-if="!scannerIsReady">Loading the scanner...</div>
+          <StreamBarcodeReader v-show="showScannerField" @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+          <div v-if="showPleaseSelectColumnMessage" class="text-left text-wrap mt-2 text-[#e65100] text-xs">
+            Please select a column
+          </div>
+          <div v-if="showScannerIsLoadingMessage" class="text-left text-wrap mt-2 text-[#e65100] text-xs">
+            Loading the scanner...
+          </div>
         </div>
       </div>
     </a-modal>
-
-    <!-- <p v-if="entry">Entry found: {{ entry.name }}</p> -->
   </div>
 </template>
