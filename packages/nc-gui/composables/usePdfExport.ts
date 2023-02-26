5f9a7a56424faf7ca0c7a7e98970c0d865b0c8f1
Daniel Spaude <daniel@spaude.de>
2023-02-26 17:36:58 +0100

    pdf export - WIP (refactoring)

--- packages/nc-gui/composables/usePdfExport.ts
+++ packages/nc-gui/composables/usePdfExport.ts
@@ -9,6 +9,226 @@ import { useI18n } from '#imports'
 
 export function usePdfExport() {
   const { t } = useI18n()
+
+  const marginBottomDefault = 20
+
+  const defaultValueStyle = {
+    marginBottom: marginBottomDefault,
+    style: {
+      bold: false,
+      fontSize: 10,
+    },
+  }
+
+  const simpleValueRendering = (cellValue: string): Content => ({
+    text: `${cellValue}`,
+    ...defaultValueStyle,
+  })
+
+  const getContentDefinitionForColumn = (col: ColumnType, cellValue: any) => {
+    console.log('FOO getContentDefinitionForColumn - col', col)
+    switch (col.uidt) {
+      case UITypes.SingleLineText: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.LongText: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.Number: {
+        return simpleValueRendering(cellValue)
+      }
+      // case UITypes.Attachment: {
+      //   const imgAttachments = cellValue?.filter?.((attObj: any) => isImage(attObj.attObj, attObj.mimetype))
+      //   if (!imgAttachments?.length) {
+      //     break
+      //   }
+      //   console.log('imgAttachments', imgAttachments)
+      //   for (let imgAttachmentIdx = 0; imgAttachmentIdx < imgAttachments.length; imgAttachmentIdx++) {
+      //     const imgAttachment = imgAttachments[imgAttachmentIdx]
+      //     if (!imgAttachment) {
+      //       continue
+      //     }
+      //     const imgDataUrl = await toDataURL(imgAttachment.url)
+      //     console.log('FOO imgDataUrl', imgDataUrl)
+      //     // { qr: 'text in QR' },
+
+      //     docDefinitionContent.push({
+      //       image: imgDataUrl,
+      //       width: 100,
+      //     })
+
+      //   }
+      //   // const firstImgAttachment = imgAttachments?.[0]
+      //   // if (!firstImgAttachment) {
+      //   //   break
+      //   // }
+
+      //   break
+      // }
+      case UITypes.SingleSelect: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.MultiSelect: {
+        const multiSelectValuesAsListContentConfig: Content = {
+          ul: cellValue.split(',').map((multiSelectValue: string) => ({
+            text: `${multiSelectValue}`,
+            marginBottom: 3,
+            style: {
+              bold: false,
+              fontSize: 10,
+            },
+          })),
+          marginBottom: marginBottomDefault,
+        }
+        return multiSelectValuesAsListContentConfig
+      }
+      case UITypes.Checkbox: {
+        const checkboxValueAsYesOrNo = cellValue ? 'Yes' : 'No'
+        return simpleValueRendering(checkboxValueAsYesOrNo)
+      }
+      case UITypes.QrCode: {
+        return {
+          qr: cellValue,
+          eccLevel: 'M',
+          version: 4,
+          marginBottom: marginBottomDefault,
+        } as Content
+      }
+      case UITypes.Formula: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.Date: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.DateTime: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.Year: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.Time: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.PhoneNumber: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.Decimal: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.Currency: {
+        let formattedCurrency: string
+        try {
+          formattedCurrency =
+            !cellValue || isNaN(cellValue)
+              ? cellValue
+              : new Intl.NumberFormat(col?.meta?.currency_locale || 'en-US', {
+                  style: 'currency',
+                  currency: col?.meta?.currency_code || 'USD',
+                }).format(cellValue)
+        } catch (e) {
+          formattedCurrency = cellValue
+        }
+        return simpleValueRendering(formattedCurrency)
+      }
+      case UITypes.Percent: {
+        return simpleValueRendering(`${cellValue}%`)
+      }
+      case UITypes.Duration: {
+        // const duration = dayjs.duration(parseInt(cellValue), 'seconds')
+        // const days = duration.days().toString().padStart(2, '0');
+        // const hours = duration.hours().toString().padStart(2, '0');
+        // const minutes = duration.minutes().toString().padStart(2, '0');
+        // const seconds = duration.seconds().toString().padStart(2, '0');
+
+        // const formatedDuration = `${days}:${hours}:${minutes}:${seconds} (DD:HH:MM:SS))`;
+        // const formatedDuration = `${duration.format('hh:mm:ss')} (hh:mm:ss))`
+        const durationOptionId = col.meta?.duration
+        const formatedDuration = convertMS2Duration(parseInt(cellValue), durationOptionId)
+
+        return simpleValueRendering(`${formatedDuration} (${durationOptions[durationOptionId].title})`)
+        break
+      }
+      case UITypes.Rating: {
+        const ratingValueAsStars = `${cellValue} (out of ${col?.meta?.max || 5})`
+        return simpleValueRendering(ratingValueAsStars)
+        break
+      }
+      case UITypes.GeoData: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.Geometry: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.JSON: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.SpecificDBType: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.Lookup: {
+        const lookupEntriesAsListContentConfig: Content = {
+          ul: cellValue.map((lookupEntryValue: string) => ({
+            text: `${lookupEntryValue}`,
+            marginBottom: 3,
+            style: {
+              bold: false,
+              fontSize: 10,
+            },
+          })),
+          marginBottom: marginBottomDefault,
+        }
+        return lookupEntriesAsListContentConfig
+        break
+      }
+      case UITypes.LinkToAnotherRecord: {
+        let contentDefinitionForCell: Content
+        if (Array.isArray(cellValue)) {
+          contentDefinitionForCell = {
+            ul: cellValue.map((ltarEntry) => ({
+              text: `${ltarEntry.Title} (${ltarEntry.Id})`,
+              marginBottom: 3,
+              style: {
+                bold: false,
+                fontSize: 10,
+              },
+            })),
+            marginBottom: marginBottomDefault,
+          }
+        } else {
+          contentDefinitionForCell = simpleValueRendering(`${cellValue.Title} (${cellValue.Id})`)
+        }
+        return contentDefinitionForCell
+      }
+      case UITypes.Rollup: {
+        return simpleValueRendering(cellValue)
+      }
+      case UITypes.Email: {
+        return {
+          text: cellValue,
+          link: cellValue,
+          ...defaultValueStyle,
+        }
+      }
+      case UITypes.URL: {
+        return {
+          text: cellValue,
+          link: cellValue,
+          ...defaultValueStyle,
+        }
+      }
+      default: {
+        return {
+          text: `[PDF export for column type '${col.uidt}' not supported]`,
+          marginBottom: marginBottomDefault,
+          style: {
+            bold: false,
+            fontSize: 10,
+          },
+        }
+      }
+    }
+  }
+
   // const toDataURL = async (url: string): Promise<string> => {
   //   const xhr = new XMLHttpRequest()
   //   xhr.open('GET', url)
@@ -54,21 +274,6 @@ export function usePdfExport() {
   //     }
   //   ]
   // };
-
-  const marginBottomDefault = 20
-
-  const defaultValueStyle = {
-    marginBottom: marginBottomDefault,
-    style: {
-      bold: false,
-      fontSize: 10,
-    },
-  }
-
-  const simpleValueRendering = (cellValue: string): Content => ({
-    text: `${cellValue}`,
-    ...defaultValueStyle,
-  })
 
   // const svgRulerLine = {
   //   svg: '<svg width="300" height="5" viewBox="0 0 300 5"><line x1="0" x2="100%" stroke="black" stroke-width="2" /></svg>',
@@ -116,263 +321,14 @@ export function usePdfExport() {
             marginBottom: marginBottomDefault,
           })
         } else {
-          switch (col.uidt) {
-            case UITypes.SingleLineText: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.LongText: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.Number: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            // case UITypes.Attachment: {
-            //   const imgAttachments = cellValue?.filter?.((attObj: any) => isImage(attObj.attObj, attObj.mimetype))
-            //   if (!imgAttachments?.length) {
-            //     break
-            //   }
-            //   console.log('imgAttachments', imgAttachments)
-            //   for (let imgAttachmentIdx = 0; imgAttachmentIdx < imgAttachments.length; imgAttachmentIdx++) {
-            //     const imgAttachment = imgAttachments[imgAttachmentIdx]
-            //     if (!imgAttachment) {
-            //       continue
-            //     }
-            //     const imgDataUrl = await toDataURL(imgAttachment.url)
-            //     console.log('FOO imgDataUrl', imgDataUrl)
-            //     // { qr: 'text in QR' },
-
-            //     docDefinitionContent.push({
-            //       image: imgDataUrl,
-            //       width: 100,
-            //     })
-
-            //   }
-            //   // const firstImgAttachment = imgAttachments?.[0]
-            //   // if (!firstImgAttachment) {
-            //   //   break
-            //   // }
-
-            //   break
-            // }
-            case UITypes.SingleSelect: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.MultiSelect: {
-              const multiSelectValuesAsListContentConfig: Content = {
-                ul: cellValue.split(',').map((multiSelectValue: string) => ({
-                  text: `${multiSelectValue}`,
-                  marginBottom: 3,
-                  style: {
-                    bold: false,
-                    fontSize: 10,
-                  },
-                })),
-                marginBottom: marginBottomDefault,
-              }
-              docDefinitionContent.push(multiSelectValuesAsListContentConfig)
-              break
-            }
-            case UITypes.Checkbox: {
-              const checkboxValueAsYesOrNo = cellValue ? 'Yes' : 'No'
-              docDefinitionContent.push(simpleValueRendering(checkboxValueAsYesOrNo))
-              break
-            }
-            case UITypes.QrCode: {
-              docDefinitionContent.push({
-                qr: cellValue,
-                eccLevel: 'M',
-                version: 4,
-                marginBottom: marginBottomDefault,
-              })
-              break
-            }
-            case UITypes.Formula: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.Date: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.DateTime: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.Year: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.Time: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.PhoneNumber: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.Decimal: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.Currency: {
-              let formattedCurrency: string
-              try {
-                formattedCurrency =
-                  !cellValue || isNaN(cellValue)
-                    ? cellValue
-                    : new Intl.NumberFormat(col?.meta?.currency_locale || 'en-US', {
-                        style: 'currency',
-                        currency: col?.meta?.currency_code || 'USD',
-                      }).format(cellValue)
-              } catch (e) {
-                formattedCurrency = cellValue
-              }
-              docDefinitionContent.push(simpleValueRendering(formattedCurrency))
-              break
-            }
-            case UITypes.Percent: {
-              docDefinitionContent.push(simpleValueRendering(`${cellValue}%`))
-              break
-            }
-            case UITypes.Duration: {
-              // const duration = dayjs.duration(parseInt(cellValue), 'seconds')
-              // const days = duration.days().toString().padStart(2, '0');
-              // const hours = duration.hours().toString().padStart(2, '0');
-              // const minutes = duration.minutes().toString().padStart(2, '0');
-              // const seconds = duration.seconds().toString().padStart(2, '0');
-
-              // const formatedDuration = `${days}:${hours}:${minutes}:${seconds} (DD:HH:MM:SS))`;
-              // const formatedDuration = `${duration.format('hh:mm:ss')} (hh:mm:ss))`
-              const durationOptionId = col.meta?.duration
-              const formatedDuration = convertMS2Duration(parseInt(cellValue), durationOptionId)
-              console.log('BAR col', col)
-
-              docDefinitionContent.push(simpleValueRendering(`${formatedDuration} (${durationOptions[durationOptionId].title})`))
-              break
-            }
-            case UITypes.Rating: {
-              const ratingValueAsStars = `${cellValue} (out of ${col?.meta?.max || 5})`
-              docDefinitionContent.push(simpleValueRendering(ratingValueAsStars))
-              break
-            }
-            case UITypes.GeoData: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.Geometry: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.JSON: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.SpecificDBType: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.Lookup: {
-              const lookupEntriesAsListContentConfig: Content = {
-                ul: cellValue.map((lookupEntryValue: string) => ({
-                  text: `${lookupEntryValue}`,
-                  marginBottom: 3,
-                  style: {
-                    bold: false,
-                    fontSize: 10,
-                  },
-                })),
-                marginBottom: marginBottomDefault,
-              }
-              docDefinitionContent.push(lookupEntriesAsListContentConfig)
-              // docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.LinkToAnotherRecord: {
-              console.log('bar LinkToAnotherRecord - cellValue', cellValue)
-              console.log('bar LinkToAnotherRecord - col', col)
-              let contentDefinitionForCell: Content
-              if (Array.isArray(cellValue)) {
-                // formattedCellVaue = cellValue.map((linkToAnotherRecordValue: any) => `${linkToAnotherRecordValue.Title} (${linkToAnotherRecordValue.Id})`).join(', ')
-                contentDefinitionForCell = {
-                  ul: cellValue.map((ltarEntry) => ({
-                    text: `${ltarEntry.Title} (${ltarEntry.Id})`,
-                    marginBottom: 3,
-                    style: {
-                      bold: false,
-                      fontSize: 10,
-                    },
-                  })),
-                  marginBottom: marginBottomDefault,
-                }
-              } else {
-                contentDefinitionForCell = simpleValueRendering(`${cellValue.Title} (${cellValue.Id})`)
-              }
-              docDefinitionContent.push(contentDefinitionForCell)
-              break
-            }
-            case UITypes.Rollup: {
-              docDefinitionContent.push(simpleValueRendering(cellValue))
-              break
-            }
-            case UITypes.Email: {
-              docDefinitionContent.push({
-                text: cellValue,
-                link: cellValue,
-                ...defaultValueStyle,
-              })
-              break
-            }
-            case UITypes.URL: {
-              docDefinitionContent.push({
-                text: cellValue,
-                link: cellValue,
-                ...defaultValueStyle,
-              })
-              break
-            }
-            default: {
-              docDefinitionContent.push({
-                text: `[PDF export for column type '${col.uidt}' not supported]`,
-                marginBottom: marginBottomDefault,
-                style: {
-                  bold: false,
-                  fontSize: 10,
-                },
-              })
-              break
-            }
-          }
+          const contentDefinitionForCell = getContentDefinitionForColumn(col, cellValue)
+          docDefinitionContent.push(contentDefinitionForCell)
         }
-        // if (colIdx === 0) {
-        //   // alert('foo')
-        //   docDefinitionContent.push(svgRulerLine)
-        // }
       }
     }
 
     const docDefinition: TDocumentDefinitions = {
       content: docDefinitionContent,
-      // header: function (currentPage, pageCount, pageSize) {
-      //   // you can apply any logic and return any valid pdfmake element
-
-      //   return [
-      //     {
-      //       text: 'Table Name // 142 Rows exported',
-      //       // alignment: (currentPage % 2) ? 'left' : 'right',
-      //       marginTop: 10,
-      //       marginLeft: 10,
-      //     },
-      //     // { canvas: [{ type: 'rect', x: 170, y: 32, w: pageSize.width - 170, h: 40, color: '#abcdef' }] }
-      //   ]
-      // },
-      // pageBreakBefore: function (currentNode, followingNodesOnPage, nodesOnNextPage, previousNodesOnPage) {
-      //   currentNode.
-      //   return currentNode.headlineLevel === 1 && followingNodesOnPage.length === 0;
-      // }
     }
 
     return docDefinition
